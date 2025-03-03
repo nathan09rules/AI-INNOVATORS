f208706bded99ecec43afeaa33bf200edb14a06a
Nathan <dsouzanathan09@gmail.com>
2025-03-03 20:44:32 +0400

    add symptom finding and scrol

--- static/script.js
+++ static/script.js
@@ -1,30 +1,34 @@
 console.log("start");
 
+let data;
+let symptoms = [];
+let symptoms_by_body = {};
+
+async function load_symptoms(){
+    const response = await fetch('/static/data.json');
+    data = await response.json();
+
+    symptoms = data.symptoms;
+    symptoms_by_body = data.body_parts;
+
+    const datalist = document.getElementById("symptoms");
+    symptoms.forEach(symptom => {
+        const option = document.createElement("option");
+        option.value = symptom;
+        datalist.appendChild(option);
+    });
+}
+load_symptoms();
+
 const is_form_there = document.getElementById("form");
 if (is_form_there){
 
-    let symptoms = [];
-
-    async function load_symptoms(){
-        const response = await fetch('/static/data.json');
-        symptoms = await response.json();
-        symptoms = symptoms.symptoms
-        const datalist = document.getElementById("symptoms");
-        symptoms.forEach(symptom => {
-            const option = document.createElement("option");
-            option.value = symptom;
-            datalist.appendChild(option);
-            
-        });
-    }
-    load_symptoms();
-
     document.getElementById("add").onclick = function(){
         const ninput = document.createElement("input");
-        ninput.setAttribute('list' ,  "symptoms");
         ninput.classList.add('inputed');
+        ninput.setAttribute('list' ,  "symptoms");
         ninput.placeholder = "symptom";
-
+        
         document.getElementById('input_container').appendChild(ninput);
     }
 
@@ -47,6 +51,21 @@ if (is_form_there){
             document.getElementById("result").textContent = data.result;
         })
     }
+}else{
+    document.getElementById("search_symp").onsubmit = function(e){
+        e.preventDefault();
+
+        const inputed = document.getElementById("s_symp").value;
+        target = document.getElementById(inputed.slice(1));
+
+        if(target){
+            target.scrollIntoView();
+        }else{
+            alert("not found")
+        }
+
+
+    }
 }
 //all the components done 
 
@@ -59,11 +78,29 @@ document.body.append(bdDIV);
 
 const shader = document.querySelector(".shader");
 const human = document.querySelector(".human_body");
+const info = document.getElementById("info");
+const part = document.getElementById("body_part");
 
-human.addEventListener("click" , (event) => {
-    const rect = human.getBoundingClientRect();
+const human_rect = human.getBoundingClientRect();
+const scaleX = human_rect.width / human.naturalWidth;
+const scaleY = human_rect.height / human.naturalHeight;
+
+function body_clicked(event, body_part) {
     const x = event.clientX;
     const y = event.clientY;
+    shader.style.clipPath = `circle(10px at ${x}px ${y}px)`;
 
-    shader.style.clipPath = `circle(50px at ${x}px ${y}px)`;
-})
+    part.textContent = body_part;
+
+    let body_part_symp = symptoms_by_body[body_part] || [];
+    let size = 0;
+    info.innerHTML = body_part_symp.map((symptom, index) => {
+        size += symptom.length + 4;
+        loca = document.getElementById(symptom) ? ("<a href='#" + symptom + "'>" + symptom + "</a>" ): symptom
+        if (size > 50){ // change value
+            size = 0;
+            return loca + "<br>"
+        }
+        return  loca + "    "
+    }).join("") ;
+}
