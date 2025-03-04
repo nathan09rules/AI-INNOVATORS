3c64ad7ca229bf230bd739347ad05a2f3704a338
Nathan <dsouzanathan09@gmail.com>
2025-03-04 19:00:33 +0400

    new ui and better and preety

--- static/script.js
+++ static/script.js
@@ -4,12 +4,17 @@ let data;
 let symptoms = [];
 let symptoms_by_body = {};
 
+let symptoms_by_age_gender = {};
+let age;
+let gender;
+
 async function load_symptoms(){
     const response = await fetch('/static/data.json');
     data = await response.json();
 
     symptoms = data.symptoms;
     symptoms_by_body = data.body_parts;
+    symptoms_by_age_gender = data.age_gender;
 
     const datalist = document.getElementById("symptoms");
     symptoms.forEach(symptom => {
@@ -52,6 +57,7 @@ if (is_form_there){
         })
     }
 }else{
+
     document.getElementById("search_symp").onsubmit = function(e){
         e.preventDefault();
 
@@ -63,8 +69,30 @@ if (is_form_there){
         }else{
             alert("not found")
         }
+    }
+
+    document.getElementById("page1").onsubmit = function(e){
+        e.preventDefault()
+
+        const input_age = document.getElementById("age").value;
+        if (input_age < 3){age = "Infant";}
+        else if (input_age < 13){age = "Child";}
+        else if (input_age < 20){age = "Teen";}
+        else if (input_age < 60){age = "Adult";}
+        else {age = "Senior"}
+        gender = document.getElementById("gender").value;
 
 
+        document.getElementById("page1").style.display = "none";
+        document.getElementById("page2").style.display = "block";
+
+        /* TRANSITION IF NEEDED
+        document.getElementById("page1").classList.add("move_up");
+        setTimeout(() => {
+            document.getElementById("page1").style.display = "none";
+            document.getElementById("page2").style.display = "block";
+        },400);
+        */
     }
 }
 //all the components done 
@@ -90,10 +118,17 @@ function body_clicked(event, body_part) {
     const y = event.clientY;
     shader.style.clipPath = `circle(10px at ${x}px ${y}px)`;
 
-    part.textContent = body_part;
+    part.textContent = `${x - human_rect.left} px by ${y - human_rect.top} px`;
 
-    let body_part_symp = symptoms_by_body[body_part] || [];
+    let body_part_symp = symptoms_by_body[body_part] ;
+
+    /* Gender class not enough data but irt works
+    let gender_age_symp = new Set(symptoms_by_age_gender[age][gender]);
+    let symp = body_part_symp.filter(i => gender_age_symp.has(i))
+    */
+
     let size = 0;
+
     info.innerHTML = body_part_symp.map((symptom, index) => {
         size += symptom.length + 4;
         loca = document.getElementById(symptom) ? ("<a href='#" + symptom + "'>" + symptom + "</a>" ): symptom
@@ -103,4 +138,10 @@ function body_clicked(event, body_part) {
         }
         return  loca + "    "
     }).join("") ;
+}
+
+function move_up(id_hide , id_show){
+    document.getElementById(id_hide).style.display = "none";
+    document.getElementById(id_show).style.display = "block";
+
 }
